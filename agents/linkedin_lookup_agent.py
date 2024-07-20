fec567b3d1928d879e4b33b8c2221d99739efc2d
Boris <borisculjak@gmail.com>
2024-07-20 14:31:36 +0200

    Created a search agent

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,5 +1,13 @@
+import sys
 import os
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+
 from dotenv import load_dotenv
+
+from tools.tools import get_profile_url_tavily
 
 load_dotenv()
 
@@ -11,4 +19,30 @@ from langchain import hub
 
 
 def lookup(name: str) -> str:
-    return ""
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    template = """given the full name {name_of_person} I want you to get me a link to their Linkedin profile page. Your answer should contain only a URL"""
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    tools_for_agent = [
+        Tool(
+            name="Craw Google for linkedin profile page",
+            func=get_profile_url_tavily,
+            description="useful for when you need to get the Linkedin Page URL",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linked_profile_url = result["output"]
+    return linked_profile_url
+
+
+print(lookup("Boris Culjak"))
